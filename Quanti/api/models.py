287d037860cecbd0f43415c94e78457167e41d74
extremlada <125869224+extremlada@users.noreply.github.com>
2025-01-30 09:16:10 +0000

    Created the Models, and the Serializers for the API

--- Quanti/api/models.py
+++ Quanti/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class reszleg(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    név = models.TextField(max_length=255, blank=False)
+    
+    def __str__(self):
+        return self.név
+    
+
+class raktar(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    név = models.TextField(max_length=255, blank=False)
+    részleg = models.ForeignKey(reszleg, blank=False, related_name="raktarok", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.név
+
+class items(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    név = models.TextField(max_length=255, blank=False)
+    barcode = models.IntegerField( blank=False)
+    Description = models.TextField(max_length=255, blank=True)
+    Mennyiség = models.IntegerField(default=1, blank=False)
+    Depot = models.ForeignKey(raktar, blank=False, related_name="Tárgyak", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        self.név
