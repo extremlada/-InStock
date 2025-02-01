93a559965829fb30f88f30f8a0aeeab65edbc8d0
extremlada <zsbecser@gmail.com>
2025-02-01 14:16:09 +0100

    részlegek api view kész, csak a tárgyakat kell létrehozni

--- Quanti/api/serializer.py
+++ Quanti/api/serializer.py
@@ -2,22 +2,24 @@ from rest_framework import serializers
 from .models import items, raktar, reszleg
 
 
-class RészlegSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = reszleg
-        fields = ['id', 'name']
-
-class RaktárSerializer(serializers.ModelSerializer):
-    raktarok = RészlegSerializer(many=True, required=False, read_only=True)
-
-    class Meta:
-        model = raktar
-        fields = ['id', 'name', 'raktarok']
-    
 
 class ItemsSerializer(serializers.ModelSerializer):
-    Tárgyak = RaktárSerializer(many=True, required=False, read_only=True)
 
     class Meta:
         model = items
         fields = ['id', 'name', 'barcode', 'description', 'mennyiség', 'Tárgyak']
+
+
+class RaktárSerializer(serializers.ModelSerializer):
+    Tárgyak = ItemsSerializer(many=True, required=False, read_only=True)
+    class Meta:
+        model = raktar
+        fields = ['id', 'name', 'részleg', 'Tárgyak']
+
+
+class RészlegSerializer(serializers.ModelSerializer):
+    raktarok = RaktárSerializer(many=True, required=False, read_only=True)
+    class Meta:
+        model = reszleg
+        fields = ['id', 'name', 'raktarok']
+
