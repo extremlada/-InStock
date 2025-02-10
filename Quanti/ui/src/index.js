1515577b982a13d5e0875e27d18eb6ae654b1a36
extremlada <zsbecser@gmail.com>
2025-02-10 04:06:41 +0100

    Created a simple UI TBC

--- Quanti/ui/src/index.js
+++ Quanti/ui/src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './components/app';
+
+const appDiv = document.getElementById("app");
+console.log('appDiv:', appDiv); // Debugging line
+if (appDiv) {
+  const root = createRoot(appDiv);
+  console.log('Rendering App component'); // Debugging line
+  root.render(<App name="tim" />);
+} else {
+  console.error('No element with id "app" found.');
+}
