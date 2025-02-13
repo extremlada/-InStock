da8b31dc232f6bc8892f7487277b63e824e0aab0
extremlada <zsbecser@gmail.com>
2025-02-13 09:08:21 +0100

    Add item details page and update routing; enhance depot creation UI

--- Quanti/api/urls.py
+++ Quanti/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ReszlegView, RaktarView
+from .views import ReszlegView, RaktarView, ItemsView  
 
 urlpatterns = [
     path("reszleg/", ReszlegView.as_view(), name="asd"),
     #path('reszleg/<uuid:id>/', Részleg_details, name='Részleg_pontosított_adatok'),
     path('raktar/', RaktarView.as_view(), name='Raktár_Lista'),
+    path('items/', ItemsView.as_view(), name='Items_Lista')
 ]
